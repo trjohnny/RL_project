7b02c4c471c3003c9c2b2e5d26682619443592e5
thoma <rossimelthomas@gmail.com>
2023-06-23 19:02:20 +0200

    Added the n step back case. Corrected some minor mistakes

--- agents/off_policy/a2c_n_step_back_agent.py
+++ agents/off_policy/a2c_n_step_back_agent.py
@@ -6,7 +6,7 @@ from tensorflow.python.keras import layers, Model
 from agent import Agent
 
 
-class A2CNStepAheadActor(Model):
+class A2CNStepBackActor(Model):
     def __init__(self, policy_model, log_std, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.policy_model = policy_model
@@ -31,7 +31,7 @@ class A2CNStepAheadActor(Model):
         return means, std_devs
 
 
-class A2CNStepAheadAgent(Agent):
+class A2CNStepBackAgent(Agent):
     def __init__(self, *agent_params, n_steps=5, entropy_coeff=1e-2):
         super().__init__(*agent_params)
 
@@ -68,7 +68,6 @@ class A2CNStepAheadAgent(Agent):
 
         self.hidden_layer = layers.Dense(self.units_per_layer_critic, activation='relu')(self.input_layer)
         self.hidden_layer = layers.Dense(self.units_per_layer_critic, activation='relu')(self.hidden_layer)
-
         self.output_layer = layers.Dense(self.action_shape[0])(self.hidden_layer)
 
         self.value_function = Model(inputs=self.input_layer, outputs=self.output_layer)
@@ -79,66 +78,29 @@ class A2CNStepAheadAgent(Agent):
         action = np.random.normal(loc=means, scale=std_devs)
         return action.flatten()
 
-    def n_step_reward(self, curr_state, done, reward, env, agent):
-
-        state_id = env.save_state()
-        curr_gamma = self.gamma
-        n_step_reward = reward
-        actual_steps = 0
-
-        for i in range(self.n_steps - 1):
-
-            if done:
-                break
-
-            action = agent.act(tf.convert_to_tensor([curr_state], dtype='float32'))
-            curr_state, reward, done, _, _ = env.step(action)
-
-            if env.unwrapped.spec.id == "PandaPushDense-v3":
-                reward -= 0.5 * np.linalg.norm(curr_state['observation'][:3] - curr_state['achieved_goal'])
-                reward -= 0.1 * np.linalg.norm(action) ** 2
-
-            if env.unwrapped.spec.id == "PandaPushDense-v3":
-                curr_state = np.concatenate([curr_state['observation'][:9], curr_state['desired_goal']], dtype=np.float32)
-            else:
-                curr_state = np.concatenate([curr_state['observation'][:6], curr_state['desired_goal']], dtype=np.float32)
-
-            n_step_reward += curr_gamma * tf.convert_to_tensor(reward, dtype='float32')
-
-            curr_gamma *= self.gamma
-
-            actual_steps += 1
-
-        env.restore_state(state_id)
-        env.remove_state(state_id)
-
-        return n_step_reward, done, tf.convert_to_tensor([curr_state]), actual_steps
-
     @tf.function
-    def __train(self, state, action, reward, next_state, done, grad_clip, entropy_coeff=0.00):
-        action_index = int(((action+1)*self.discrete_values)/2)
+    def train(self, state, action, n_steps_reward, next_state, done, steps_ahead, entropy_coeff=0.008, grad_clip=-1):
         with tf.GradientTape(persistent=True) as tape:
-
-            pre_means = self.actor(state)  # array of tensors
-            # pre_means is a vector of tensors, each tensor is a vector of probabilities
+            means, std_devs = self.actor(state)
             value = self.critic(state)
+
             next_value = self.critic(next_state)
 
-            indices = tf.stack([tf.range(pre_means.shape[0]), action_index], axis=1)
-            action_probability = tf.gather_nd(pre_means, indices)
-
-            advantage = reward + (1 - done) * self.gamma * next_value - value
+            action_prob = tf.exp(-0.5 * tf.square((action - means) / std_devs)) / (std_devs * tf.sqrt(2. * np.pi))
+            action_prob = tf.reduce_prod(action_prob, axis=1, keepdims=True)
+            advantage = n_steps_reward + (1 - done) * self.gamma ** steps_ahead * next_value - value
 
             # Compute the entropy of the current policy
-            entropy = -tf.reduce_sum(pre_means * tf.math.log(pre_means + tf.keras.backend.epsilon()), axis=-1)
+            entropy = 0.5 * (tf.math.log(2 * np.pi * std_devs ** 2) + 1)
+            entropy = tf.reduce_sum(entropy, keepdims=True)
 
             # Compute the actor loss
-            actor_loss = -tf.reduce_mean(tf.math.log(tf.maximum(action_probability, 5e-6)) * tf.stop_gradient(
-                advantage) + entropy_coeff * entropy)
+            actor_loss = -tf.reduce_mean(
+                tf.math.log(tf.maximum(action_prob, 5e-6)) * tf.stop_gradient(advantage) + entropy_coeff * entropy)
 
             # Compute the critic loss
-            critic_loss = tf.reduce_mean(
-                tf.square(tf.stop_gradient(reward + (1 - done) * self.gamma * next_value) - value))
+            critic_loss = tf.reduce_mean(tf.square(
+                tf.stop_gradient(n_steps_reward + (1 - done) * self.gamma ** steps_ahead * next_value) - value))
 
         # Compute and apply the gradients for the actor loss
         actor_grads = tape.gradient(actor_loss, self.actor.trainable_variables)
@@ -161,6 +123,7 @@ class A2CNStepAheadAgent(Agent):
         # Clear the tape to free up resources
         del tape
 
+
     def train(self, state, action, reward, next_state, done):
         return
 
@@ -177,9 +140,14 @@ class A2CNStepAheadAgent(Agent):
 
         total_reward = 0
         done = False
+        truncated = False
         num_step = 0
 
-        while not done and num_step < 50:
+        states = [state]
+        actions = []
+        rewards = []
+
+        while not done and not truncated:
             action = self.act(state)
             next_state, reward, done, truncated, info = env.step(action)
 
@@ -189,7 +157,7 @@ class A2CNStepAheadAgent(Agent):
             if env.unwrapped.spec.id == "PandaPushDense-v3":
                 reward -= 0.5 * np.linalg.norm(next_state['observation'][:3] - next_state['achieved_goal'])
 
-                reward -= 0.1 * np.linalg.norm(action) ** 2
+                reward -= 0.05 * np.linalg.norm(action) ** 2
 
             if env.unwrapped.spec.id == "PandaPushDense-v3":
                 next_state = np.concatenate([next_state['observation'][:9], next_state['desired_goal']], dtype=np.float32)
@@ -198,19 +166,21 @@ class A2CNStepAheadAgent(Agent):
 
             action = tf.convert_to_tensor(action, dtype='float32')
             reward = tf.convert_to_tensor(reward, dtype='float32')
-
-            n_steps_reward, done_cur, n_step_state, actual_steps = self.n_step_reward(next_state, done, reward,
-                                                                                      env, self)
-
             next_state = tf.convert_to_tensor([next_state], dtype='float32')
-            n_step_state = tf.convert_to_tensor(n_step_state, dtype='float32')
-            n_steps_reward = tf.convert_to_tensor(n_steps_reward, dtype='float32')
+            done = tf.convert_to_tensor(done, dtype='float32')
 
-            train(state, action, reward, next_state, n_steps_reward, done_cur, n_step_state, actual_steps + 1)
+            states.append(next_state)
+            actions.append(action)
+            rewards.append(reward)
+
+            if len(states) > steps_ahead:
+                self.train(states[-steps_ahead - 1], actions[-steps_ahead],
+                           sum([rewards[-(steps_ahead - i)] * self.gamma ** i for i in range(steps_ahead)]),
+                           next_state, done, steps_ahead)
+
             state = next_state
             total_reward += reward
             num_step += 1
-
 
         if not done:
             self.finished = episode
